1c50e6ca151bec7c3a3e0a2a29dec0de3def78d5
mohamadGazer <mohamadgazer@yahoo.com>
2025-07-01 17:14:43 +0300

    - add files and folders [done]

---
+++ admin/apiRoutes.js
@@ -0,0 +1,16 @@
+// apiRoutes.js
+
+export const API_BASE_URL = "http://localhost:9000/api";
+
+export const endpoints = {
+  login: "/login",
+  logout: "/logout",
+  me: "/me",
+  users: "/users",
+  products: "/products",
+  orders: "/orders",
+  categories: "/categories",
+  cartItems: "/cart-items",
+  checkout: "/checkout",
+  laptopDetails: "/laptop-details",
+};
